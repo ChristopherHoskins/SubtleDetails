0c38ad4ef9e8cfdc6adaee413c8f149a000b47f5
ChrisLeeHoskins <chrisleehoskins@gmail.com>
2018-03-12 14:29:54 -0700

    Unreal start project
    The beginning of a long journey

---
+++ BeginningProject/Source/BeginningProject/BeginningProject.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BeginningProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BeginningProject, "BeginningProject" );
