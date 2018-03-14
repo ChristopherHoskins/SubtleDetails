f7552f65e42ab93784906635bedf989c27fdcdb2
ChrisLeeHoskins <chrisleehoskins@gmail.com>
2018-03-13 23:28:05 -0700

    Ray cast to determine grab distances

--- BeginningProject/Source/BeginningProject/Grabber.cpp
+++ BeginningProject/Source/BeginningProject/Grabber.cpp
@@ -6,6 +6,7 @@
 #include "GameFramework//PlayerController.h"
 
 #include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -36,10 +37,23 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
   FVector viewLocation;
   FRotator viewRotation;
   GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(viewLocation, viewRotation);
-  UE_LOG(LogTemp, Warning, TEXT("Location: %s Rotation: %s"), *viewLocation.ToString(), *viewRotation.ToString())
+  //UE_LOG(LogTemp, Warning, TEXT("Location: %s Rotation: %s"), *viewLocation.ToString(), *viewRotation.ToString())
+
+  // Draw a red trace in the world to visual
+  FVector unitVector = FVector(1.0f, 1.0f, 1.0);
+  FVector LineTraceEnd = viewLocation + viewRotation * unitVector * reach;
 
   // Ray-cast out to reach distance
-
+  DrawDebugLine(
+    GetWorld(),
+    viewLocation,
+    LineTraceEnd,
+    FColor(255, 0, 0),
+    false,
+    0.0f,
+    0.0f,
+    10.0f
+  );
 
   // See what we hit
 
