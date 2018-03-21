86ab3e78be638b14be205b95814807a4a95965d1
ChrisLeeHoskins <chrisleehoskins@gmail.com>
2018-03-20 21:23:27 -0700

    Etc

--- BeginningProject/Source/BeginningProject/OpenDoor.cpp
+++ BeginningProject/Source/BeginningProject/OpenDoor.cpp
@@ -5,6 +5,8 @@
 #include "Engine/World.h"
 #include "GameFramework/Actor.h"
 #include "GameFramework/PlayerController.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/PrimitiveComponent.h"
 #include "Grabber.h"
 
 // Sets default values for this component's properties
@@ -13,10 +15,7 @@ UOpenDoor::UOpenDoor()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
-
 
 // Called when the game starts
 void UOpenDoor::BeginPlay()
@@ -24,39 +23,29 @@ void UOpenDoor::BeginPlay()
 	Super::BeginPlay();
 
   owner = GetOwner();
+  if (!owner)
+    return;
+  if (!pressurePlate)
+  {
+    UE_LOG(LogTemp, Error, TEXT("Missing pressure plate on %s"), *GetOwner()->GetName())
+  }
   originalYaw = owner->GetActorRotation().Yaw;
-}
-
-void UOpenDoor::OpenDoor()
-{
-  // Set the door rotation
-  owner->SetActorRotation(FRotator(0.0f, openAngle, 0.0f));
-}
-
-void UOpenDoor::CloseDoor()
-{
-  // Set the door rotation
-  owner->SetActorRotation(FRotator(0.0f, originalYaw, 0.0f));
 }
 
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+  Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// Poll the trigger volume
+  // Poll the trigger volume
   // If the actorThatOpens is in the volume
-  if (GetTotalMassOfActorsOnPlate() > 50.0f)
+  if (GetTotalMassOfActorsOnPlate() > triggerMass)
   {
-    OpenDoor();
-    lastDoorOpenTime = GetWorld()->GetTimeSeconds();
+    onOpenRequest.Broadcast();
   }
-
-
-  // Check if its time to close the door
-  if (GetWorld()->GetTimeSeconds() - lastDoorOpenTime > doorCloseDelay)
+  else
   {
-    CloseDoor();
+    onCloseRequest.Broadcast();
   }
 }
 
@@ -66,10 +55,15 @@ float UOpenDoor::GetTotalMassOfActorsOnPlate()
 
   // Find all overlapping actors
   TArray<AActor*> overlappingActors;
+
+  if (!pressurePlate) { return totalMass; }
   pressurePlate->GetOverlappingActors(OUT overlappingActors);
-
+  
   // Iterate through them adding their masses
-
+  for (const auto& actor : overlappingActors)
+  {
+    totalMass += actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
+  }
 
   return totalMass;
 }
